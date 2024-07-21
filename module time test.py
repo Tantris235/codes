36ae1c7e95e3d9b35b41ac72f566f77ce7a5fabb
Tantris235 <166976348+Tantris235@users.noreply.github.com>
2024-07-21 17:19:19 +0200

    Delete module time test.py

--- module time test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-teraz = datetime.datetime.now()
-rok = teraz.year
-dzien = teraz.day
-sekundy = teraz.second
-minuty = teraz.minute
-miesiąc = teraz.month
-czas = rok, miesiąc, dzien, minuty, sekundy
-print ("Teraz jest rok : ", rok, "\nTeraz jest miesiąc : ", miesiąc, "\nTeraz jest dzien : ", dzien, "\nTeraz jest minuta : ", minuty, "\nTeraz jest sekunda : ", sekundy, "\nA Razem : ", czas)
